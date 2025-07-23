eb7f3f54e5e563c977c8e6bf9346312e5072d37f
kalilmasters <krmas2003@gmail.com>
2025-07-22 22:51:10 -0400

    listed adventure types on user profile
    saved info to database

--- lib/Profile.dart
+++ lib/Profile.dart
@@ -1,3 +1,4 @@
+import 'dart:collection';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -7,6 +8,8 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
+import 'home.dart';
+
 class Profile extends StatefulWidget{
   const Profile({super.key});
 
@@ -14,8 +17,14 @@ class Profile extends StatefulWidget{
   State<Profile> createState() => _ProfileState();
 }
 
+
 class _ProfileState extends State<Profile>{
   String? _username;
+
+  Map<String, bool>? hasAdventureTypes;
+  List<String>? adventureTypes = ['hiking', 'canoeing', 'rock climbing', 'cave diving'];
+
+  
   String _error = "";
   SharedPreferences? notCookies;
   dynamic _userIcon;
@@ -27,23 +36,148 @@ class _ProfileState extends State<Profile>{
     {
       notCookies = nc;
       _username = notCookies!.getString('username');
+      fetchAdventureTypes().then((_){
+        setState(() {
+          "";
+        });
+      });
+      setState(() {
+        //DO NOT DELETE. Required to run Setstate
+        "";
+      });
     });
+  }
+
+  Future fetchAdventureTypes() async {
+    final doc = await FirebaseFirestore.instance.collection('users').doc(_username).get();
+
+    if (doc.exists){
+      final raw = doc.data()?['adventureTypes'];
+      if (raw is Map<String, dynamic>) {
+        hasAdventureTypes = raw.map((key, value) => MapEntry(key, value as bool));
+      }
+    }
+
+    if (hasAdventureTypes == null){
+      hasAdventureTypes = {};
+    }
+      
+    for(var key in adventureTypes!)
+      if(!hasAdventureTypes!.containsKey(key)){
+        hasAdventureTypes![key] = false;
+      }
+  }
+  Future saveAdventureTypes() async {
+    print("Saving Adventure Types...");
+    await FirebaseFirestore.instance.collection('users').doc(_username).set(
+    {
+      'adventureTypes': hasAdventureTypes
+    },
+      SetOptions(merge: true));
+    
+    print("saved Adventure Types");
+  
   }
   
   @override
   Widget build(BuildContext context) {
+    if (hasAdventureTypes == null) 
+    {
+      return Scaffold
+      (
+        appBar: AppBar(title: Text('Rovaly')),
+        body: Center(child: CircularProgressIndicator()),
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text("Rovaly"),
       ),
-      body: Center(child:  Row(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           //Display User Icon,
-          Text(_username?? "DefaultUser"),
-          TextButton(onPressed: () {throw Exception("Not Implemented");}, child: Text("Edit"))
+          Text((_username?? "DefaultUser") + "| Adventure Type Preferences"),
+          Row(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(adventureTypes![0] + ": "),
+              TextButton(onPressed: () 
+              {
+                if (adventureTypes != null && hasAdventureTypes != null && adventureTypes!.isNotEmpty) {
+                  final key = adventureTypes![0];
+                   hasAdventureTypes![key] = !(hasAdventureTypes![key] ?? false);
+                }
+                setState(() {
+                  "";
+                });
+              }, 
+              child: Text(hasAdventureTypes![adventureTypes![0]].toString()))
+            ],
+          
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(adventureTypes![1] + ": "),
+              TextButton(onPressed: () 
+              {
+                if (adventureTypes != null && hasAdventureTypes != null && adventureTypes!.isNotEmpty) {
+                  final key = adventureTypes![1];
+                   hasAdventureTypes![key] = !(hasAdventureTypes![key] ?? false);
+                }
+                setState(() {
+                  "";
+                });
+              }, 
+              child: Text(hasAdventureTypes![adventureTypes![1]].toString()))
+            ],
+          
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(adventureTypes![2] + ": "),
+              TextButton(onPressed: () 
+              {
+                if (adventureTypes != null && hasAdventureTypes != null && adventureTypes!.isNotEmpty) {
+                  final key = adventureTypes![2];
+                   hasAdventureTypes![key] = !(hasAdventureTypes![key] ?? false);
+                }
+                setState(() {
+                  "";
+                });
+              }, 
+              child: Text(hasAdventureTypes![adventureTypes![2]].toString()))
+            ],
+          
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(adventureTypes![3] + ": "),
+              TextButton(onPressed: () 
+              {
+                if (adventureTypes != null && hasAdventureTypes != null && adventureTypes!.isNotEmpty) {
+                  final key = adventureTypes![3];
+                   hasAdventureTypes![key] = !(hasAdventureTypes![key] ?? false);
+                }
+                setState(() {
+                  "";
+                });
+              }, 
+              child: Text(hasAdventureTypes![adventureTypes![3]].toString()))
+            ],
+          
+          ),
+          TextButton(onPressed: () {saveAdventureTypes().then((_) {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => Home(),
+                  ));
+          });}, child: Text("Apply Changes"))
         ],
-      ))
+      )
     );
+    
   }
 }
